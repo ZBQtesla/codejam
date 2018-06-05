5b778730e7c0701c7183205f7287f60630889d97
TangYucopper <TangYucopper@users.noreply.github.com>
2018-06-05 23:34:21 +0800

    codejam 2016 qualification Round,Problem B

--- 2016/Qualification Round/Problem B/Python3/solve_pancakes.py
+++ 2016/Qualification Round/Problem B/Python3/solve_pancakes.py
@@ -17,4 +17,30 @@ def writeOutput(outfile,output):
 def compute(pattern):
     if pattern == len(pattern) * '+':
         return 0
-    
+    lastneg = pattern.rfind('-')
+    pattern = pattern[:lastneg + 1]
+    if pattern.startswith('+'):
+        for i in range(0,len(pattern)):
+            if pattern[i] != '+':
+                position = i
+                break
+        return compute((position) * '-' + pattern[position:]) + 1
+    else:
+        return compute(neg(pattern[::-1])) + 1
+
+def neg(string):
+    listS = list(string)
+    for i in range(len(listS)):
+        listS[i] = '-' if listS[i] == '+' else '+'
+    return ''.join(listS)
+
+def main():
+    inputList = getInput('B-large-practice.in')
+
+    output = []
+    for element in inputList:
+        output.append(compute(element))
+
+    writeOutput('solve_B-large-practice.txt',output)
+
+main()
