3c266c6a94d0229ccca37c621537977af3a4cdb2
zbqtesla <17766589280@163.com>
2018-07-04 10:25:04 +0800

    Optimized leetcode-4

--- BQCreated/leetcode/4/Solution.java
+++ BQCreated/leetcode/4/Solution.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
-
+import java.util.*;
 /**
  * @author baiqiang
  */
-
 public class Solution {
   private static final double E = 0.000001;
 
@@ -13,61 +11,34 @@ public class Solution {
   public double findMedianSortedArrays(int[] nums1, int[] nums2) {
     int m = nums1.length;
     int n = nums2.length;
-    if(m == 0 && n != 0){
-      return getMedian(nums2);
+
+    // Base case
+    if(m <= 2 || n <= 2){
+      if(m > 2){
+        nums1 = Arrays.copyOfRange(nums1, (m - 3) / 2, (m + 4) / 2);
+      }
+      if(n > 2){
+        nums2 = Arrays.copyOfRange(nums2, (n - 3) / 2, (n + 4) / 2);
+      }
+      List<Integer> result = new ArrayList<>();
+      for(int e : nums1){
+        result.add(e);
+      }
+      for(int e : nums2){
+        result.add(e);
+      }
+      Collections.sort(result);
+      return (result.get(result.size() / 2) + result.get((result.size() - 1) / 2)) / 2.0;
     }
-    if(m != 0 && n == 0){
-      return getMedian(nums1);
-    }
-    if(m == 0 && n == 0){
-      return 0;
-    }
+
     double median1 = getMedian(nums1);
     double median2 = getMedian(nums2);
-
-    // Termination condition
-    if (Math.abs(median1 - median2) < E) {
+    if(Math.abs(median1 - median2) < E){
       return median1;
     }
 
-    if (m == 1 || n == 1) {
-      if(m == 1 && n == 1){
-        return (nums1[0] + nums2[0]) / 2.0;
-      }
-      int single = 0;
-      int[] num = null;
-      if (m == 1) {
-        single = nums1[0];
-        num = nums2;
-      } else {
-        single = nums2[0];
-        num = nums1;
-      }
-      int len = num.length;
-      num = Arrays.copyOfRange(num, (len / 2) - 1, (len / 2) + 1 + (len % 2));
-      int index = num.length;
-      len = num.length;
-      for (int i = 0; i < len; i++) {
-        if (num[i] > single) {
-          index = i;
-          break;
-        }
-      }
-      int[] result = new int[len + 1];
-      for (int i = 0; i < index; i++) {
-        result[i] = num[i];
-      }
-      result[index] = single;
-      if(index != len){
-        for (int i = index + 1; i < len + 1; i++) {
-          result[i] = num[i - 1];
-        }
-      }
-      return getMedian(result);
-    }
-
     // Divide
-    int minSize = m < n ? m / 2 : n / 2;
+    int minSize = m < n ? (m - 1) / 2 : (n - 1) / 2;
     return median1 < median2
         ? findMedianSortedArrays(Arrays.copyOfRange(nums1, minSize, m),
             Arrays.copyOfRange(nums2, 0, n - minSize))
